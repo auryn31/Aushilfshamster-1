5162da7b0237daf2863c0e444f83a0d9fd9dea46
Kolya Opahle <kolyaoaphle@gmail.com>
2020-03-21 21:11:21 +0100

    fixed title bar decoration

--- sources/corona_karma/lib/widgets/titleBar.dart
+++ sources/corona_karma/lib/widgets/titleBar.dart
@@ -13,17 +13,11 @@ class TitleBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(children: [
-      Align(
-          alignment: Alignment(0.0, 0.5),
-          child: SizedBox(
-            height: 12,
-            width: MediaQuery.of(context).size.width / 2.25,
-            child: DecoratedBox(
-                decoration:
-                    BoxDecoration(color: Color.fromARGB(255, 255, 206, 84))),
-          )),
-      Center(child: Text(titleText, style: Styles.navbarTitle))
-    ]);
+    return Container(
+        decoration: BoxDecoration(
+            color: Color.fromARGB(255, 255, 206, 84),
+            borderRadius: BorderRadius.all(Radius.circular(10))),
+        padding: EdgeInsets.all(4),
+        child: Text(titleText, style: Styles.navbarTitle));
   }
 }
